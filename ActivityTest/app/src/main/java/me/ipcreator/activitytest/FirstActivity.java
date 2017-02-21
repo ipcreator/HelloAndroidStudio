4e22ce1860b13abd75eaefc5abda6aaf2c8606a8
IPCreator <zhuxuanlv@126.com>
2017-02-21 16:58:30 +0800

    complete practice

--- ActivityTest/app/src/main/java/me/ipcreator/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/me/ipcreator/activitytest/FirstActivity.java
@@ -1,7 +1,12 @@
 package me.ipcreator.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,15 +14,65 @@ import android.widget.Toast;
 public class FirstActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked add menu_item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked remove menu_item", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+                //String data = "Hello SecondActivity";
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                //Intent intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel:10086"));
+                // Intent intent = new Intent(Intent.ACTION_VIEW);
+                // intent.setData(Uri.parse("https://github.com/ipcreator/HelloAndroidStudio"));
+                // Intent intent = new Intent("me.ipcreator.activitytest.ACTION_START");
+                // intent.addCategory("me.ipcreator.activitytest.MY_CATEGORY");
+                // Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+                // Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                //finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity",returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
