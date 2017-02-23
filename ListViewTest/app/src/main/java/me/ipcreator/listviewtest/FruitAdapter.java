680e26457f31711437f3a8c2db09403067097104
IPCreator <zhuxuanlv@126.com>
2017-02-23 12:44:23 +0800

    Init for run

---
+++ ListViewTest/app/src/main/java/me/ipcreator/listviewtest/FruitAdapter.java
@@ -0,0 +1,64 @@
+package me.ipcreator.listviewtest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by user on 2/23/2017.
+ */
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Fruit fruit = getItem(position);
+        View view;
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder= new ViewHolder();
+            viewHolder.fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView)view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        }else{
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView)view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+
+        return view;
+    }
+
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+
+        resourceId = textViewResourceId;
+
+    }
+
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+    }
+}
