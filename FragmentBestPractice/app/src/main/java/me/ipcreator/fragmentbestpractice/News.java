c92e4e405bd91c6f4491df29bd48f43f97bee0d7
IPCreator <zhuxuanlv@126.com>
2017-02-24 14:28:16 +0800

    Init for run

---
+++ FragmentBestPractice/app/src/main/java/me/ipcreator/fragmentbestpractice/News.java
@@ -0,0 +1,27 @@
+package me.ipcreator.fragmentbestpractice;
+
+/**
+ * Created by user on 2/24/2017.
+ */
+
+public class News {
+
+    private String title;
+    private String content;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setContent(String content){
+        this.content=content;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
